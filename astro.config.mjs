ec74d2c964b310c7c5b6424808df4236d9da18a0
kenono-dancer <narutaku@yahoo.co.jp>
2026-04-05 00:56:21 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://itxdancer.com', // ← 【ここを追加！】ルートドメインを指定
   integrations: [react()],
   vite: { plugins: [tailwindcss()] },
   trailingSlash: 'ignore',
